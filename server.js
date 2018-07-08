18eaba396225a4ef634b797af19f79fb99dec969
auxtin <aleonardo2@ucmerced.edu>
2018-07-08 13:05:16 -0700

    Added message saying which port is live

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8000);
 
-console.log("Starting server on locahost:8000 . . . .");
+console.log("Starting server on locahost:8000");
